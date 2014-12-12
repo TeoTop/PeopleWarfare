80b43519325bd47a8347e06dedae5cb7640c2357
theo <tchapon@insa-rennes.fr>
2014-12-12 18:31:31 +0100

    wrapper sans unsafe

--- Diagramme de classe code/Implementation/StrategieCarte.cs
+++ Diagramme de classe code/Implementation/StrategieCarte.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using Wrapper;
 
@@ -18,33 +19,19 @@ namespace PeopleWar
         /**
          * Creates a map which contains the same number of each box
          */
-        unsafe public void creerCarte()
+        public void creerCarte()
         {
-            /*Random rnd = new Random();
-            
-            int[] nb = new int[4] {0,0,0,0};
-
-            bool drap = false;
-            for (int i = 0, typeCase = 0; i < nbCase; i++)
-            {
-                drap = false;
-                while (!drap)
-                {
-                    typeCase = rnd.Next(0, 4);
-                    if (nb[typeCase] < nbCase / 4)
-                    {
-                        nb[typeCase] += 1;
-                        drap = true;
-                    }
-                }
-                cases.Add((CaseA)FabriqueCase.INSTANCE.getCase((EnumCase)typeCase));
-            }*/
             WrapperAlgos w = new WrapperAlgos();
-            int* cases_wrapper = w.generer_carte(nbCase, Enum.GetNames(typeof(EnumCase)).Length);
+            int[] cases_tmp = new int[nbCase];
+            IntPtr cases_wrapper = w.generer_carte(nbCase, Enum.GetNames(typeof(EnumCase)).Length);
+            Marshal.Copy(cases_wrapper, cases_tmp, 0, nbCase);
+            
             for (int i = 0; i < nbCase; i++)
             {
-                cases.Add((CaseA)FabriqueCase.INSTANCE.getCase((EnumCase)cases_wrapper[i]));
+                cases.Add((CaseA)FabriqueCase.INSTANCE.getCase((EnumCase)cases_tmp[i]));
             }
+            
+            
         }
 
 
--- Wrapper/Wrapper.h
+++ Wrapper/Wrapper.h
@@ -11,9 +11,19 @@ namespace Wrapper {
 	public:
 		WrapperAlgos() { algos = Algos_New(); }
 		~WrapperAlgos() { Algos_Delete(algos); }
-		int* generer_carte(int nbCase, int nbTypeCase) { return algos->generer_carte(nbCase, nbTypeCase); }
-		int* placer_joueurs(int nbCase) { return algos->placer_joueurs(nbCase); }
-		int** suggestion_cases(int** cases, int taille, int* posEnnemi, int posActuelle, int typeUnite) { return algos->suggestion_cases(cases, taille, posEnnemi, posActuelle, typeUnite); }
+		IntPtr generer_carte(int nbCase, int nbTypeCase) { return System::IntPtr(algos->generer_carte(nbCase, nbTypeCase)); }
+		IntPtr placer_joueurs(int nbCase) { return System::IntPtr(algos->placer_joueurs(nbCase)); }
+		IntPtr suggestion_cases(array<int, 2>^ cases, int taille, array<int>^ posEnnemi, int posActuelle, int typeUnite) {
+			pin_ptr<int> p_posEnnemi = &posEnnemi[0];
+
+			pin_ptr<int> c1 = &cases[0,0];
+			pin_ptr<int> c2 = &cases[1,0];
+			int* c3[2];
+			c3[0] = c1;
+			c3[1] = c2;
+			int** c4 = c3;
+			return System::IntPtr(algos->suggestion_cases(c4, taille, p_posEnnemi, posActuelle, typeUnite));
+		}
 		int** cases_atteignable(int* cases, int* posEnnemi, int posActuelle, int typeUnite, int pm) { return algos->cases_atteignable(cases, posEnnemi, posActuelle, typeUnite, pm); }
 	};
 }
