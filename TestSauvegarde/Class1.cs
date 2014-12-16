b043710f03c4b33ced6c08a3a040006df674202d
TeoTop <theo.c24@hotmail.fr>
2014-12-16 13:54:26 +0100

    Sauvegarde et chargement

---
+++ TestSauvegarde/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSauvegarde
+{
+    [Serializable]
+    public class Class1
+    {
+        public int nbTourMax { get; set; }
+
+        public List<Class2> tours { get; set; }
+
+        public Class3 carte { get; set; }
+
+        public Class1(int nbTourMax, List<Class2> tours, Class3 carte)
+        {
+            this.nbTourMax = nbTourMax;
+            this.tours = tours;
+            this.carte = carte;
+        }
+
+        public Class1(int nbTourMax)
+        {
+            this.nbTourMax = nbTourMax;
+            this.tours = new List<Class2>();
+            this.carte = new Class3();
+        }
+
+        public void ajouterTour(String txt)
+        {
+            tours.Add(new Class2(txt));
+        }
+    }
+}
